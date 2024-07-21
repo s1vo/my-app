bfb3fda54fad5e13fac1d86f17c1ce8938f6fcaa
Michael Sivokon <s1vo13@yandex.ru>
2024-07-21 20:24:55 +0300

    без использования JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,39 @@
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
     const currentYear = new Date().getFullYear();
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p> Edit <code>src/App.js</code> and save to reload.</p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                {/* Начало декларативного стиля */}
-                <p>{currentYear}</p>
-                {/* Конец декларативного стиля */}
-            </header>
-        </div>
+
+    return createElement(
+        'div',
+        { className: 'App' },
+        createElement(
+            'header',
+            { className: 'App-header' },
+            createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+            createElement(
+                'p',
+                null,
+                'Edit ',
+                createElement('code', null, 'src/App.js'),
+                ' and save to reload.'
+            ),
+            createElement(
+                'a',
+                {
+                    className: 'App-link',
+                    href: 'https://reactjs.org',
+                    target: '_blank',
+                    rel: 'noopener noreferrer'
+                },
+                'Learn React'
+            ),
+            createElement('p', null, currentYear)
+        )
     );
 }
 
 export default App;
+
+
