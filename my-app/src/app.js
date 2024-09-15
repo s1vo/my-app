5f8c14462b9662a37efd91cddc2627c88c79dae8
s1vo13 <sivokonma@gmail.com>
2024-09-15 20:16:54 +0300

    Список дел через Axios

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,84 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import styles from './app.module.css';
-import data from './data.json';
-import { useState } from 'react';
 
-export const App = () => {
-	// Initialize state for steps and active index
-	const [steps] = useState(data);
-	const [activeIndex, setActiveIndex] = useState(0);
+const App = () => {
+	const [todos, setTodos] = useState([]);
 
-	// Handlers for navigation
-	const backStep = () => {
-		if (activeIndex > 0) {
-			setActiveIndex(activeIndex - 1);
-		}
-	};
-
-	const nextStep = () => {
-		if (activeIndex < steps.length - 1) {
-			setActiveIndex(activeIndex + 1);
-		} else {
-			setActiveIndex(0);
-		}
-	};
-
-	const resetSteps = () => {
-		setActiveIndex(0);
-	};
-
-	// Flags for first and last step
-	const isFirstStep = activeIndex === 0;
-	const isLastStep = activeIndex === steps.length - 1;
+	useEffect(() => {
+		axios.get('https://jsonplaceholder.typicode.com/todos')
+			.then(response => {
+				setTodos(response.data.slice(0, 20)); // Отображаем первые 10 дел
+			})
+			.catch(error => console.error('Error fetching the todos:', error));
+	}, []);
 
 	return (
 		<div className={styles.container}>
-			<div className={styles.card}>
-				<h1>Инструкция по готовке пельменей</h1>
-				<div className={styles.steps}>
-					<div className={styles['steps-content']}>
-						{/* Display the content of the active step */}
-						<h2>{steps[activeIndex].title}</h2>
-						<p>{steps[activeIndex].content}</p>
-					</div>
-					<ul className={styles['steps-list']}>
-						{/* Render the steps */}
-						{steps.map((step, index) => (
-							<li
-								key={step.id}
-								className={`${styles['steps-item']} ${
-									index <= activeIndex ? styles.done : ''
-								} ${index === activeIndex ? styles.active : ''}`}
-							>
-								<button
-									className={styles['steps-item-button']}
-									onClick={() => setActiveIndex(index)}
-								>
-									{index + 1}
-								</button>
-								Шаг {index + 1}
-							</li>
-						))}
-					</ul>
-					<div className={styles['buttons-container']}>
-						<button
-							className={styles.button}
-							onClick={backStep}
-							disabled={isFirstStep}
-						>
-							Назад
-						</button>
-						{isLastStep ? (
-							<button className={styles.button} onClick={resetSteps}>
-								Сначала
-							</button>
-						) : (
-							<button className={styles.button} onClick={nextStep}>
-								Далее
-							</button>
-						)}
-					</div>
-				</div>
-			</div>
+			<h1>Todo List</h1>
+			<ul>
+				{todos.map(todo => (
+					<li key={todo.id} className={todo.completed ? 'styles.completed' : ''}>
+						{todo.title}
+						<button>X</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
+
+export default App;
